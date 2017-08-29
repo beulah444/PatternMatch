c829adf337f7528d44a15e3db4666d2046836782
beulah444 <beulah444@gmail.com>
2017-08-28 17:43:02 -0700

    Add project files.

---
+++ PatternMatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        //need to detect 525,528,530 to 599
+        string pattern = @"\b(52[5,8])|(5[3-9][0-9])\b";
+
+        string input = "600 789 558 524 525 526 527 528 531 530 540 541 555 543 567 560  570 598 599 888";
+
+        foreach (Match match in Regex.Matches(input, pattern))
+            Console.WriteLine("'{0}' found at position {1}.", match.Value, match.Index);
+
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
